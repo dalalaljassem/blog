daddc78b23e8d38ae3420fff21bb450261a197dc
Dalal <dalal.aljassem@gmail.com>
2020-12-05 19:27:34 +0300

    newest

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class Post(models.Model):
    
     title = models.CharField(max_length=250)
-    header_image = models.ImageField(upload_to="images/")
+    # header_image = models.ImageField(upload_to="images/")
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now())
     published_date = models.DateTimeField(blank=True, null=True)
