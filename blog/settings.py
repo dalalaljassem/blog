856231e1df6d6bab43bf404ef2f37c470b23ff42
Dalal <dalal.aljassem@gmail.com>
2020-12-05 19:24:07 +0300

    newest

--- blog/settings.py
+++ blog/settings.py
@@ -126,4 +126,4 @@ STATIC_URL ='/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'main/static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
